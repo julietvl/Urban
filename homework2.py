b9f896d4b9ad81f389705ed34c95cf2c0d29739c
Julia Gorbunova <45624396+julietvl@users.noreply.github.com>
2024-07-02 18:33:03 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homeworks = 12
+times = 1.5
+course = 'Python'
+times_task = times / homeworks
+print('Курс:', course, ', всего задач:', homeworks,', затрачено часов:', times, ', среднее время выполнения', times_task, 'часа')
